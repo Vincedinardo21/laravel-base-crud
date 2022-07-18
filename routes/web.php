2cb9344f3981b98f911db4535d95826c8e910c3d
Vincedinardo21 <vincedinardo21@gmail.com>
2022-07-18 17:58:45 +0200

    Show comic

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@home')->name('home');
 
-Route::get('/comics', 'ComicsController@index')->name('comics.index');
-Route::post('/comics', 'ComicsController@store')->name('comics.store');
+// Route::get('/comics', 'ComicsController@index')->name('comics.index');
+// Route::post('/comics', 'ComicsController@store')->name('comics.store');
+
+Route::resource('/comics', 'ComicsController');
